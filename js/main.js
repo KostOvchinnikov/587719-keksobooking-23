f734ad291817eb23cbe623673ee805f617603b0a
KostOvchinnikov <kocyh31@gmail.com>
2021-06-12 19:06:50 +0500

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,36 +1,24 @@
 const getRandomInt = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  let int;
 
-  if (min < 0) {
+  if ((min < 0) | (max < 0) | (min >= max)) {
     return undefined;
-  } else if (max < 0) {
-    return undefined;
-  } else {
-    int = Math.floor(Math.random() * (max - min + 1)) + min;
-    return int;
   }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 getRandomInt(0, 100);
 
 const getRandomNum = function (min, max, simbolsAfterComma) {
-  let num;
+  min = Math.ceil(min);
 
-  if (min < 0) {
+  if ((min < 0) | (max < 0) | (min >= max)) {
     return undefined;
-  } else if (max < 0) {
-    return undefined;
-  } else {
-    num = Math.random() * (max - min) + min;
   }
 
-  if (!num) {
-    undefined;
-  }
-
-  return num.toFixed(simbolsAfterComma);
+  return Number((Math.random() * (max - min) + min).toFixed(simbolsAfterComma));
 };
 
 getRandomNum(0, 100, 2);
